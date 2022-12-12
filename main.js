5333181a59c448e35541858dadd36e4a201d05c2
Ana Victoria <anavictoriagomes1810@gmail.com>
2022-12-12 16:38:01 -0300

    Definindo todos os elementos do robô para somar e subtrair os valores das peças

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
-const subtrair = document.querySelector("#subtrair");
-const somar = document.querySelector("#somar");
-const braco = document.querySelector("#braco");
+// const subtrair = document.querySelector("#subtrair");
+// const somar = document.querySelector("#somar");
+// const braco = document.querySelector("#braco");
 
 const controle = document.querySelectorAll(".controle-ajuste");
 
+
+// Função para pegar todos os elementos
+controle.forEach((elemento) => {
+    elemento.addEventListener("click", (evento) => {
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
+        console.log()
+    })
+})
+
 // Função ao clicar no botão
-somar.addEventListener("click", () => { manipulaDados("somar") })
-subtrair.addEventListener("click", () => { manipulaDados("subtrair") })
+// somar.addEventListener("click", () => { manipulaDados("somar") })
+// subtrair.addEventListener("click", () => { manipulaDados("subtrair") })
 
 
 // Função para manipular os dados ao clicar no botão
-function manipulaDados(operacao) {
-    if (operacao === "subtrair") {
-        braco.value = parseInt(braco.value) - 1;
+function manipulaDados(operacao, controle) {
+    const peca = controle.querySelector(".controle-contador")
+    console.log()
+
+    if (operacao === "-") {
+        peca.value = parseInt(peca.value) - 1;
     } else {
-        braco.value = parseInt(braco.value) + 1;
+        peca.value = parseInt(peca.value) + 1;
     }
 }
+
+// 
