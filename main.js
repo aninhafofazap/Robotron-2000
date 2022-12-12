09bd266ab99cb6e71d819303f72499bb7ac7d426
Ana Victoria <anavictoriagomes1810@gmail.com>
2022-12-12 13:54:19 -0300

    Iniciando a manipulação do DOM, aprendendo a utilizar o eventListener

---
+++ main.js
@@ -0,0 +1,15 @@
+const robotron = document.querySelector("#robotron")
+
+robotron.addEventListener("click", (evento) => {
+    console.log("Cliquei no robô.")
+})
+
+
+
+function dizOi() {
+    console.log("oi")
+    console.log("Bem-vindo ao Robotron 2000")
+}
+
+
+dizOi("Pedro")
